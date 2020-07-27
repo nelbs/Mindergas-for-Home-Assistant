1a1382dcfbf9a458465be034bf329133bf745ef6
Nick Elbers <nick.elbers@rhdhv.com>
2020-07-27 10:56:03 +0200

    Update sensor.py

--- custom_components/mindergas/sensor.py
+++ custom_components/mindergas/sensor.py
@@ -2,7 +2,9 @@
 import logging
 import datetime
 import json
+import re as re
 import voluptuous as vol
+import requests
 
 from homeassistant.util import dt
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -10,7 +12,6 @@ from homeassistant.const import (
     ATTR_ATTRIBUTION, CONF_NAME, CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_USERNAME)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.restore_state import RestoreEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -32,56 +33,31 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 	username = config.get(CONF_USERNAME)
 	password = config.get(CONF_PASSWORD)
 	name = config.get(CONF_NAME)
-	add_entities([GasPrognose(username, password, name)], True)
-	add_entities([GasUsed(username, password, name)], True)
-	add_entities([GraadDag(username, password, name)], True)
 
-class GasPrognose(RestoreEntity):
-	def __init__(self, username, password, name):
-		# initialiseren sensor
+	try:
+		data = GasData(username, password)
+		add_entities([GasSensor(data, name + '_used', 1, True)], True)
+		add_entities([GasSensor(data, name + '_prognose', 9, True)], True)
+		add_entities([GasSensor(data, name + '_graaddag', 5, True)], True)
+
+	except requests.exceptions.HTTPError as error:
+		_LOGGER.error(error)
+	return False
+
+class GasData(object):
+	def __init__(self, username, password):
+		self.data = None
 		self._username = username
 		self._password = password
-		self._name = name + '_prognose'
-		self._state = 0
-		self._attributes = {'last_update': None}
-		self.update()
-		
-	@property
-	def name(self):
-		return self._name
-
-	@property
-	def unit_of_measurement(self):
-	# Return the unit of measurement of this entity, if any.
-		return 'm3'
-
-	@property
-	def state(self):
-		return self._state
-
-	@property
-	def device_state_attributes(self):
-	# Return the state attributes.
-		return self._attributes
-
-	@property
-	def icon(self):
-	# Icon to use in the frontend.
-		return 'mdi:chart-line'
 
 	def update(self):
-		import requests
 		from lxml import html
 		from bs4 import BeautifulSoup
 		LOGIN_URL = "https://www.mindergas.nl/users/sign_in/"
-		URL_DATA = "https://www.mindergas.nl/member/year_overview/new"
-		URL_DASHBOARD = "https://www.mindergas.nl/member/dashboard"
-		URL_RESULT = 'none'
-		n = 0
-		while not URL_RESULT == URL_DASHBOARD :
-			if n == 10:
-				_LOGGER.error('Update of ' + str(self._name) + 'failed after ' + n + 'attempts')
-				break
+		URL = "https://www.mindergas.nl/member/year_overview/new"
+		_LOGGER.debug('Scraping mindergas.nl')
+
+		try:
 			session_requests = requests.session()
 
 			# Get login csrf token
@@ -97,36 +73,24 @@ class GasPrognose(RestoreEntity):
 
 			# Perform login
 			result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
-			URL_RESULT = result.url
-			n += 1
 
-			if URL_RESULT == URL_DASHBOARD:
-				# Scrape url
-				raw_html = session_requests.get(URL_DATA, headers=dict(referer=URL_DATA)).text
-				data = BeautifulSoup(raw_html, 'html.parser')
+			# Scrape url
+			raw_html = session_requests.get(URL, headers=dict(referer=URL)).text
+			data = BeautifulSoup(raw_html, 'html.parser')
+			self.data = data
 
-				# Scrape prognose
-				div = data.find_all("div", class_="table_cell")[9]
-				result = round(eval(div.get_text().replace('m3','').replace(',' , '.').rstrip()))
-				self._attributes['last_update'] = dt.now().isoformat('T')
-				self._state = result
-			else: 
-				pass
+		except requests.exceptions.RequestException as exc:
+			_LOGGER.error('Error occurred while scraping data: %r', exc)
+			self.data = None
+			return False
 
-	async def async_added_to_hass(self) -> None:
-		"""Handle entity which will be added."""
-		await super().async_added_to_hass()
-		state = await self.async_get_last_state()
-		if not state:
-			return
-		self._state = state.state
-
-class GasUsed(RestoreEntity):
-	def __init__(self, username, password, name):
+class GasSensor(Entity):
+	def __init__(self, data, name, table_cell, round=False):
 		# initialiseren sensor
-		self._username = username
-		self._password = password
-		self._name = name + '_used'
+		self.data = data
+		self._name = name
+		self.round = round
+		self.table_cell = table_cell
 		self._state = None
 		self._attributes = {'last_update': None}
 		self.update()
@@ -155,139 +119,18 @@ class GasUsed(RestoreEntity):
 		return 'mdi:chart-line'
 
 	def update(self):
-		import requests
-		from lxml import html
-		from bs4 import BeautifulSoup
-		LOGIN_URL = "https://www.mindergas.nl/users/sign_in/"
-		URL_DATA = "https://www.mindergas.nl/member/year_overview/new"
-		URL_DASHBOARD = "https://www.mindergas.nl/member/dashboard"
-		URL_RESULT = 'none'
-		n = 0
-		while not URL_RESULT == URL_DASHBOARD :
-			if n == 10:
-				_LOGGER.error('Update of ' + str(self._name) + 'failed after ' + n + 'attempts')
-				break
-			session_requests = requests.session()
+		self.data.update()
+		data = self.data.data
+		try:
+			# Scrape data
+			div = data.find_all("div", class_="table_cell")[self.table_cell]
+			value = eval(re.sub(r'[^Z0-9-.]', "", div.get_text().replace(',', '.')))
+			if self.round == True:
+				result = round(value)
+			else:
+				result = value
+		except ValueError:
+			result = None
 
-			# Get login csrf token
-			result = session_requests.get(LOGIN_URL)
-			tree = html.fromstring(result.text)
-			authenticity_token = list(set(tree.xpath("//input[@name='authenticity_token']/@value")))[0]
-
-			# Create payload
-			payload = {
-				"user[email]": self._username,
-				"user[password]": self._password,
-				"authenticity_token": authenticity_token}
-
-			# Perform login
-			result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
-			URL_RESULT = result.url
-			n += 1
-
-			if URL_RESULT == URL_DASHBOARD:		
-				# Scrape url
-				raw_html = session_requests.get(URL_DATA, headers=dict(referer=URL_DATA)).text
-				data = BeautifulSoup(raw_html, 'html.parser')
-				
-				# Scrape gas used
-				div = data.find_all("div", class_="table_cell")[1]
-				result = round(eval(div.get_text().replace('m3','').replace(',' , '.').rstrip()))
-				self._attributes['last_update'] = dt.now().isoformat('T')
-				self._state = result
-			else: 
-				pass
-
-	async def async_added_to_hass(self) -> None:
-		"""Handle entity which will be added."""
-		await super().async_added_to_hass()
-		state = await self.async_get_last_state()
-		if not state:
-			return
-		self._state = state.state
-		
-class GraadDag(RestoreEntity):
-	def __init__(self, username, password, name):
-		# initialiseren sensor
-		self._username = username
-		self._password = password
-		self._name = name + '_graaddag'
-		self._state = None
-		self._attributes = {'last_update': None}
-		self.update()
-		
-	@property
-	def name(self):
-		return self._name
-
-	@property
-	def unit_of_measurement(self):
-	# Return the unit of measurement of this entity, if any.
-		return 'm3'
-
-	@property
-	def state(self):
-		return self._state
-
-	@property
-	def device_state_attributes(self):
-	# Return the state attributes.
-		return self._attributes
-
-	@property
-	def icon(self):
-	# Icon to use in the frontend.
-		return 'mdi:chart-line'
-
-	def update(self):
-		import requests
-		from lxml import html
-		from bs4 import BeautifulSoup
-		LOGIN_URL = "https://www.mindergas.nl/users/sign_in/"
-		URL_DATA = "https://www.mindergas.nl/member/year_overview/new"
-		URL_DASHBOARD = "https://www.mindergas.nl/member/dashboard"
-		URL_RESULT = 'none'
-		
-		n = 0
-		while not URL_RESULT == URL_DASHBOARD :
-			if n == 10:
-				_LOGGER.error('Update of ' + str(self._name) + 'failed after ' + n + 'attempts')
-				break
-			session_requests = requests.session()
-
-			# Get login csrf token
-			result = session_requests.get(LOGIN_URL)
-			tree = html.fromstring(result.text)
-			authenticity_token = list(set(tree.xpath("//input[@name='authenticity_token']/@value")))[0]
-
-			# Create payload
-			payload = {
-				"user[email]": self._username,
-				"user[password]": self._password,
-				"authenticity_token": authenticity_token}
-
-			# Perform login
-			result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
-			URL_RESULT = result.url
-			n += 1
-
-			if URL_RESULT == URL_DASHBOARD:			
-				# Scrape url
-				raw_html = session_requests.get(URL_DATA, headers=dict(referer=URL_DATA)).text
-				data = BeautifulSoup(raw_html, 'html.parser')
-
-				# Scrape graaddag
-				div = data.find_all("div", class_="table_cell")[5]
-				result = eval(div.get_text().replace('m3/graaddag', '').replace(',', '.').rstrip())
-				self._attributes['last_update'] = dt.now().isoformat('T')
-				self._state = result
-			else: 
-				pass
-			
-	async def async_added_to_hass(self) -> None:
-		"""Handle entity which will be added."""
-		await super().async_added_to_hass()
-		state = await self.async_get_last_state()
-		if not state:
-			return
-		self._state = state.state
+		self._attributes['last_update'] = dt.now().isoformat('T')
+		self._state = result
